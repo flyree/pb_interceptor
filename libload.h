0d1561a5fbd8833fd1be0e5e58c5fbf8533ad97e
Bo Fang <flyree@gmail.com>
2016-02-25 17:21:18 -0700

    bug fix

--- libload.h
+++ libload.h
@@ -16,5 +16,5 @@ extern KNOB<BOOL> is_inlib;
 UINT32 parseLibNames(string libfilename);
 VOID libLoad(RTN rtn,VOID *v);
 const char * stripPath(const char * path);
-
+extern VOID instruction_Instrumentation(INS ins, VOID *v);
 #endif //PB_INTERCEPTOR_LIBLOAD_H
