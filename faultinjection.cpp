19a200166c4468503183c207f541804147903ff7
Bo Fang <flyree@gmail.com>
2016-03-16 17:19:09 -0600

    print inst address

--- faultinjection.cpp
+++ faultinjection.cpp
@@ -516,6 +516,7 @@ VOID instruction_Instrumentation(INS ins, VOID *v){
                 cout << "SIZE: "<<size << endl;
                 cout << REG_StringShort(reg).c_str() << endl;
                 cout << INS_Disassemble(ins) << endl;
+                cout << INS_Address(ins) << endl;
 				INS_InsertIfPredicatedCall(ins, IPOINT_BEFORE, (AFUNPTR) FI_InjectMemIf, IARG_END);
 				INS_InsertThenPredicatedCall(ins, IPOINT_BEFORE, (AFUNPTR) FI_InjectFaultMemAddr,
 											 IARG_INST_PTR, IARG_REG_REFERENCE, base_reg,IARG_UINT32,size,IARG_PTR, routine_name,IARG_END);
@@ -550,6 +551,7 @@ VOID instruction_Instrumentation(INS ins, VOID *v){
             cout <<"SIZE: "<<size << endl;
             cout << REG_StringShort(reg).c_str() << endl;
             cout << INS_Disassemble(ins) << endl;
+            cout << INS_Address(ins) << endl;
 			INS_InsertIfPredicatedCall(ins, IPOINT_BEFORE, (AFUNPTR) FI_InjectMemIf, IARG_END);
 			INS_InsertThenPredicatedCall(ins, IPOINT_BEFORE, (AFUNPTR) FI_InjectFaultMemAddr,
 										 IARG_INST_PTR, IARG_REG_REFERENCE, reg,IARG_UINT32,size,IARG_PTR, routine_name, IARG_END);
