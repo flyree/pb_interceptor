bef13409f80f4e69aa4f5465f57e2b696c83d2ed
Bo Fang <flyree@gmail.com>
2016-05-24 20:37:59 -0700

    add random instruction generator

---
+++ randomInst.cpp
@@ -0,0 +1,80 @@
+//
+// Created by Bo Fang on 2016-05-18.
+//
+
+#include<iostream>
+#include<fstream>
+
+#include <set>
+#include <map>
+#include <string>
+
+#include "pin.H"
+#include "utils.h"
+
+KNOB<UINT64> randInst(KNOB_MODE_WRITEONCE, "pintool",
+                      "randinst", 0, "Total number of instructions");
+
+static UINT64 randinst = 0;
+static UINT64 allinst = 0;
+
+
+// Pin calls this function every time a new instruction is encountered
+VOID CountInst(INS ins, VOID *v)
+{
+    allinst++;
+    if (randinst == allinst)
+        cout << INS_Address(ins) << endl;
+}
+
+// bool mayChangeControlFlow(INS ins){
+// 	REG reg;
+// 	if(!INS_HasFallThrough(ins))
+// 		return true;
+// 	int numW = INS_MaxNumWRegs(ins);
+// 	for(int i =0; i < numW; i++){
+// 		if(reg == REG_RIP || reg == REG_EIP || reg == REG_IP) // conditional branches
+// 			return true;
+// 	}
+// 	return false;
+// }
+// This function is called when the application exits
+VOID Fini(INT32 code, VOID *v)
+{
+    // Write to a file since cout and cerr maybe closed by the application
+    ofstream OutFile;
+    OutFile.open(instcount_file.Value().c_str());
+    OutFile.setf(ios::showbase);
+    OutFile.close();
+}
+
+/* ===================================================================== */
+/* Print Help Message                                                    */
+/* ===================================================================== */
+
+INT32 Usage()
+{
+    cerr << "This tool counts the number of dynamic instructions executed" << endl;
+    cerr << endl << KNOB_BASE::StringKnobSummary() << endl;
+    return -1;
+}
+
+
+
+int main(int argc, char * argv[])
+{
+    PIN_InitSymbols();
+    // Initialize pin
+    if (PIN_Init(argc, argv)) return Usage();
+
+    // Register Instruction to be called to instrument instructions
+    INS_AddInstrumentFunction(CountInst, 0);
+
+    // Register Fini to be called when the application exits
+    PIN_AddFiniFunction(Fini, 0);
+
+    // Start the program, never returns
+    PIN_StartProgram();
+
+    return 0;
+}
