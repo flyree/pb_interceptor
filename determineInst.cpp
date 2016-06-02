df714242324ed27bc99c0d8e89c52d5e40f262ee
Bo Fang <flyree@gmail.com>
2016-06-01 17:21:40 -0700

    converting

--- determineInst.cpp
+++ determineInst.cpp
@@ -36,7 +36,7 @@ VOID CountInst(INS ins, VOID *v)
     randominst++;
     if (randominst!= randint.Value()){
         cout << pc.Value() << ":" << INS_Address(ins) << endl;
-        if (atoi(pc.Value().c_str())== (UINT64)INS_Address(ins)){
+        if ((UINT64)atoi(pc.Value().c_str())== (UINT64)INS_Address(ins)){
             iterations++;
         }
     }
